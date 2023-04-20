434e04af9f390625065cb4c19013e7b323f7d1bb
Letizia OBIGLIO (P) <letizia.obiglio-ext@ixxitechside.com>
2023-04-20 15:57:35 +0200

    exercice1 init

---
+++ src/App.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Welcome', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Welcome/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+// tester que l'App contient bien "Learn React"
+
+// https://testing-library.com/docs/react-testing-library/api#queries
+
+
+// Tester que nous avons bien 3 éléments button
+//https://testing-library.com/docs/queries/about
+
+// Tester que le titre du boutton est bien click me !
+
+// Tester qu'au click d'un boutton le text du header change
+// https://testing-library.com/docs/dom-testing-library/api-events
