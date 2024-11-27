021e232ea11accc66cf41364d81fe73039d2c1f4
Letizia Obiglio <letizia.obiglio-ext@ixxitechside.com>
2024-11-27 07:49:43 +0100

    fix: faute d'ortographe

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders Welcome', () => {
+test("renders Welcome", () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-
 // tester que l'App contient bien "Learn React"
-
 // https://testing-library.com/docs/react-testing-library/api#queries
-
 
 // Tester que nous avons bien 3 éléments button
 //https://testing-library.com/docs/queries/about
 
-// Tester que le titre du boutton est bien click me !
+// Tester que le titre du bouton est bien click me !
 
-// Tester qu'au click d'un boutton le text du header change
+// Tester qu'au click d'un bouton le text du header change
 // https://testing-library.com/docs/dom-testing-library/api-events
