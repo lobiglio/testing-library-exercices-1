03e574ec19ede5f4bc73906918f8d99bd4ced915
Letizia Obiglio <letizia.obiglio-ext@ixxitechside.com>
2024-11-27 11:33:02 +0100

    feat: arrow function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import { useState } from "react";
 import "./App.css";
 
-function Header({ title }) {
-  return(<h1 data-testid="title">{ title }</h1>)
-}
-function Component({ title, onClick }) {
-  
-  return(
+const Header = ({ title }) => {
+  return <h1 data-testid="title">{title}</h1>;
+};
+const Component = ({ title, onClick }) => {
+  return (
     <div className="component">
       <p>{title}</p>
       <button onClick={() => onClick(title)}>click me !</button>
-    </div>)
-}
+    </div>
+  );
+};
 
-function App() {
-  const [title, setTitle] = useState("Welcome")
-  const handleOnClick = (value) => setTitle(value) 
+const App = () => {
+  const [title, setTitle] = useState("Welcome");
+  const handleOnClick = (value) => setTitle(value);
   return (
     <div className="App">
       <header className="App-header">
         <Header title={title} />
         <Component title="Learn React" onClick={handleOnClick} />
-        <Component title="Learn Angular" onClick={handleOnClick}/>
-        <Component title="Learn Vue" onClick={handleOnClick}/>
-    </header>  
+        <Component title="Learn Angular" onClick={handleOnClick} />
+        <Component title="Learn Vue" onClick={handleOnClick} />
+      </header>
     </div>
   );
-}
+};
 
 export default App;
